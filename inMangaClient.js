f7073b9387fe117c4783e715225402e574ae2c55
GermanPachec0 <87890408+GermanPachec0@users.noreply.github.com>
2023-09-10 11:28:46 -0300

    first commit with all working

---
+++ inMangaClient.js
@@ -0,0 +1,64 @@
+import cheerio from 'cheerio';
+import axios from 'axios';
+
+export const getChapters = (id) => {
+    return new Promise((resolve, reject) => {
+        axios.get('https://inmanga.com/chapter/chapterIndexControls?identification=' + id)
+            .then(response => {
+                const $ = cheerio.load(response.data);
+                const options = $('.ChapterListClass option');
+                const result = [];
+
+                options.each((index, option) => {
+                    result.push({
+                        value: $(option).attr('value'),
+                        text: $(option).text(),
+                    });
+                });
+                resolve(result);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+                reject(error);
+            });
+    });
+}
+
+export const getChaptersDetails = async (id) => {
+    return new Promise((resolve, reject) => {
+        axios.get('https://inmanga.com/chapter/chapterIndexControls?identification=' + id)
+            .then(response => {
+                const $ = cheerio.load(response.data);
+                const selectElements = $('.PageListClass');
+
+                const options = $(selectElements[0]).find('option');
+                const pages = [];
+                options.each((index, option) => {
+                    let page = $(option).text();
+                    let value = $(option).attr('value')
+                    pages.push({
+                        value: page,
+                        page: value,
+                        img: `https://pack-yak.intomanga.com/images/manga/One-Piece/chapter/1047/page/${page}/${value}`
+
+                    });
+                });
+
+                resolve(pages);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+                reject(error);
+            });
+    });
+}
+
+export const getPages = async () => {
+    try {
+        const chapters = await getChapters('89fcccde-30c5-485d-bc7a-8dd6c6b21612');
+        const chaptersFull = await getChaptersDetails('89fcccde-30c5-485d-bc7a-8dd6c6b21612')
+        const dataChapter = { chapter: chapters[0].value, page: chapters[0].text , pages: chaptersFull }
+    } catch (error) {
+        console.error('Error in getPages:', error);
+    }
+}
---
+++ index.js
@@ -0,0 +1,41 @@
+import { getPages , getChapters , getChaptersDetails } from "./inMangaClient.js";
+
+import express, { json } from 'express' // require -> commonJS
+import { corsMiddleware } from './middlewares/cors.js'
+console.clear()
+
+const app = express()
+app.use(json())
+app.use(corsMiddleware())
+app.disable('x-powered-by')
+
+app.get('/chapters', async (req, res) => {
+    try {
+        const chapters = await getChapters('89fcccde-30c5-485d-bc7a-8dd6c6b21612');
+        res.json(chapters);
+    } catch (error) {
+        console.error('Error fetching chapters:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.get('/chapter_details/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+        const chaptersFull = await getChaptersDetails(id)
+        const dataChapter = { chapter: id, pages: chaptersFull }
+        res.json(dataChapter);
+    } catch (error) {
+        console.error('Error fetching chapters:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+const PORT = 1234
+
+
+app.listen(PORT, () => {
+  console.log(`server listening on port http://localhost:${PORT}`)
+})
+
+await getPages()
